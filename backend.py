a0447ff7ef10b2690dfa5224f28b9e99be52ea31
l0g-lab <lb@needrandom.com>
2024-05-22 00:10:20 -0400

    w.i.p.

---
+++ backend.py
@@ -0,0 +1,27 @@
+# backend.py
+
+from flask import Flask, render_template, request
+import subprocess
+
+app = Flask(__name__,template_folder='templates', static_folder='static')
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    hash_value = request.form['hash']
+    # Process the hash_value as needed
+
+    return "Received hash: " + hash_value
+
+# Route to serve the HTML page
+@app.route('/')
+def index():
+    # Run Hashcat command
+    #result = subprocess.run(['hashcat', 'options', 'file_to_crack'], capture_output=True, text=True)
+    result = subprocess.run(['hashcat', '--version'], capture_output=True, text=True)
+
+    # Extract relevant output
+    hashcat_output = result.stdout
+    return render_template('index.html', hashcat_output=hashcat_output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
