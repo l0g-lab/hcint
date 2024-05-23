cc53290fd6775615d101866b434b739d50e969fb
l0g-lab <lb@needrandom.com>
2024-05-23 00:13:25 -0400

    almost there

--- backend.py
+++ backend.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, request
 import subprocess
+import time
 
 MD5_CODE=0
 NTLMV1_CODE=5500
@@ -15,19 +16,27 @@ app = Flask(__name__,template_folder='templates', static_folder='static')
 
 @app.route('/submit', methods=['POST'])
 def submit_form():
-    hash_value = request.form['hash']
+    # Run Hashcat command
+    #proc = subprocess.run(['hashcat', '-m', REQUIRED_HASH_CODE, HASH_TO_BE_CRACKED], shell=True, stdout=subprocess.PIPE, text=True)
+
+    # Dummy scipt to emulate command output
+    proc = subprocess.Popen(['cat /var/log/README'], shell=True, stdout=subprocess.PIPE, text=True)
+
+    hc_output = ""
+    for line in iter(proc.stdout.readline,''):
+        time.sleep(0.1)
+        #yield line.decode('utf8').rstrip() + '<br/>\n'
+        hc_output += line + "<br/>\n"
+    
+    hash_value = request.form['hash_in']
     hash_type = request.form['hash_id']
 
-    # Run Hashcat command
-    # result = subprocess.run(['hashcat', '-m', REQUIRED_HASH_CODE 'file_to_crack'], capture_output=True, text=True)
-
     # Process the hash_value as needed
-    return render_template('/index.html', hash_value=request.form['hash'], hash_type=request.form['hash_id'],hc_version=hc_version)
+    return render_template('/index.html', hash_value=hash_value, hash_type=hash_type, hc_version=hc_version, hc_output=hc_output)
 
-# Route to serve the HTML page
 @app.route('/')
 def index():
-    # Generate base index.html 
+    # Generate base index page
     return render_template('index.html', hc_version=hc_version)
 
 if __name__ == '__main__':
